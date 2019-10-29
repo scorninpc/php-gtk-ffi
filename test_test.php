ca65afa6e8a0a241b75d634464f40257963a8951
Bruno P. Gonçalves <scorninpc@gmail.com>
2019-10-28 22:25:50 -0300

    more new tests with char*

---
+++ test_test.php
@@ -0,0 +1,42 @@
+<?php
+
+$ffi = FFI::cdef("
+	
+	struct test 
+	{
+		char *field_string;
+	};
+	
+", "libc.so.6");
+
+
+// Create
+$test = $ffi->new("struct test");
+
+
+$p = FFI::new("char[2]");
+$p[0] = "M";
+$p[1] = "Y";
+var_dump($p);
+echo "\n-----\n";
+
+
+
+$p = FFI::new("char");
+$pointer = FFI::addr($p);
+FFI::memset($pointer, ord("A"), FFI::sizeof($p)*10);
+FFI::memcpy($pointer, "ABCDE", FFI::sizeof($p) * 5 );
+
+
+
+echo "\n-----\n";
+var_dump($p);
+echo "\n-----\n";
+var_dump($pointer);
+echo "\n-----\n";
+
+
+
+
+// Dump all 
+// var_dump($test);
